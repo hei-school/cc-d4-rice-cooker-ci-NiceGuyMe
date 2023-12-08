7588a4902ba5b5d768b9404cb2085fe24e4bd84e
NiceGuyMe <boy-unfind@proton.me>
2023-12-08 19:29:52 +0300

    feat: init ricecooker in C#

---
+++ RiceCooker.cs
@@ -0,0 +1,60 @@
+using System;
+class RiceCooker
+{
+    private int waterLevel;
+    private int riceAmount;
+    private bool cooking;
+    private int elapsedTime;
+
+    public void AddWater(int amount)
+    {
+        waterLevel += amount;
+        Console.WriteLine($"Added {amount} cup(s) of water. Current water level: {waterLevel} cups.");
+    }
+
+    public void AddRice(int amount)
+    {
+        riceAmount += amount;
+        Console.WriteLine($"Added {amount} cup(s) of rice. Current rice amount: {riceAmount} cups.");
+    }
+
+    public void StartCooking()
+    {
+        if (waterLevel < 1 || riceAmount < 1)
+        {
+            Console.WriteLine("Please add water and rice before starting cooking.");
+        }
+        else
+        {
+            cooking = true;
+            elapsedTime = 0;
+            Console.WriteLine("Cooking started!");
+        }
+    }
+
+    public void CheckStatus()
+    {
+        if (cooking)
+        {
+            elapsedTime += 1;
+            if (elapsedTime < 10)
+            {
+                Console.WriteLine($"Cooking... ({elapsedTime} minutes elapsed)");
+            }
+            else if (elapsedTime == 10)
+            {
+                Console.WriteLine("Cooking complete! Enjoy your rice!");
+                cooking = false;
+            }
+            else
+            {
+                Console.WriteLine("Rice overcooked. Please try again.");
+                cooking = false;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Rice cooker is not currently cooking.");
+        }
+    }
+}
---
+++ RiceCookerSimulation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+public class RiceCookerSimulation
+{
+    public static void Main(string[] args)
+    {
+        RiceCooker riceCooker = new RiceCooker();
+
+        while (true)
+        {
+            // Display menu options
+            Console.WriteLine("\nRice Cooker Menu:");
+            Console.WriteLine("1. Add Water");
+            Console.WriteLine("2. Add Rice");
+            Console.WriteLine("3. Start Cooking");
+            Console.WriteLine("4. Check Status");
+            Console.WriteLine("5. Exit");
+
+            // Get user input
+            int choice = int.Parse(Console.ReadLine());
+
+            // Execute chosen option
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter water amount (cups): ");
+                    int waterAmount = int.Parse(Console.ReadLine());
+                    riceCooker.AddWater(waterAmount);
+                    break;
+                case 2:
+                    Console.Write("Enter rice amount (cups): ");
+                    int riceAmount = int.Parse(Console.ReadLine());
+                    riceCooker.AddRice(riceAmount);
+                    break;
+                case 3:
+                    riceCooker.StartCooking();
+                    break;
+                case 4:
+                    riceCooker.CheckStatus();
+                    break;
+                case 5:
+                    Console.WriteLine("Exiting rice cooker program.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+
+            // Delay for visual effect
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
