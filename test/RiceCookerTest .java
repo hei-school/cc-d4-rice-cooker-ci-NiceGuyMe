e1708d267a810b50ea46effec2bbdca9b1957117
NiceGuyMe <boy-unfind@proton.me>
2023-12-17 17:30:43 +0300

    feat: add unit test

---
+++ test/RiceCookerTest .java
@@ -0,0 +1,57 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RiceCookerTest {
+
+    @Test
+    public void testAddWater() {
+        RiceCooker riceCooker = new RiceCooker();
+        riceCooker.addWater(2);
+        assertEquals(2, riceCooker.getWaterLevel());
+    }
+
+    @Test
+    public void testAddRice() {
+        RiceCooker riceCooker = new RiceCooker();
+        riceCooker.addRice(3);
+        assertEquals(3, riceCooker.getRiceAmount());
+    }
+
+    @Test
+    public void testStartCooking() {
+        RiceCooker riceCooker = new RiceCooker();
+        riceCooker.addWater(2);
+        riceCooker.addRice(3);
+        riceCooker.startCooking();
+        assertTrue(riceCooker.isCooking());
+    }
+
+    @Test
+    public void testStartCookingWithoutIngredients() {
+        RiceCooker riceCooker = new RiceCooker();
+        riceCooker.startCooking();
+        assertFalse(riceCooker.isCooking());
+    }
+
+    @Test
+    public void testCheckStatusWhileCooking() {
+        RiceCooker riceCooker = new RiceCooker();
+        riceCooker.addWater(2);
+        riceCooker.addRice(3);
+        riceCooker.startCooking();
+        riceCooker.checkStatus();
+        assertTrue(riceCooker.getElapsedTime() > 0);
+    }
+
+    @Test
+    public void testCheckStatusAfterCooking() {
+        RiceCooker riceCooker = new RiceCooker();
+        riceCooker.addWater(2);
+        riceCooker.addRice(3);
+        riceCooker.startCooking();
+        for (int i = 0; i < 10; i++) {
+            riceCooker.checkStatus();
+        }
+        assertFalse(riceCooker.isCooking());
+    }
+}
